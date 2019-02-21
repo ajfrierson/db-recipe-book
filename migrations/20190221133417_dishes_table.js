3fe69256fe13c45db1aa42f3dbc387e65771f692
ajfrierson <alvinfrierson@fuller.edu>
2019-02-21 14:04:43 -0800

    updated and saved seeding and migration with knex to implement changes to tables.

--- migrations/20190221133417_dishes_table.js
+++ migrations/20190221133417_dishes_table.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('dishes', tbl => {
+
+    tbl.increments();
+
+    tbl.string('name', 155).notNullable().unique().defaultTo('Not Provided');
+  });
 };
 
 exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExits('dishes');
   
 };
--- migrations/20190221133519_recipes_table.js
+++ migrations/20190221133519_recipes_table.js
@@ -1,8 +1,23 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('recipes', tbl => {
+      
+    tbl.increments();
+
+    tbl.string('name', 155).notNullable();
+    tbl.string('instructions').notNullable();
+
+    tbl
+        .integer('dish_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('dishes')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('recipes');
 };
